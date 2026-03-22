30ae959d872cf9e77bffa5d4a6e71e60b48bc6c1
Isaac Rico <isaacarnell.rico@gmail.com>
2026-03-21 19:58:34 -0700

    sales overview to monthly sales + additions to sales overview

--- E-Commerce Analysis/olist_analytics/models/mart/mart_olist__sales_overview.sql
+++ E-Commerce Analysis/olist_analytics/models/mart/mart_olist__sales_overview.sql
@@ -1,28 +1,80 @@
--- Grain: one row per month
+-- Grain: one row per order
 
 with orders_enriched as (SELECT * FROM {{ ref('int_olist__orders_enriched') }}),
 order_items_enriched as (SELECT * FROM {{ ref('int_olist__order_items_enriched') }}),
 
-monthly_sales as (
+order_items as (
     select 
-        date_trunc('month', order_purchase_timestamp) as month,
-        date_trunc('year', order_purchase_timestamp) as year,
-        sum(total_payment_value) as total_revenue,
-        count(distinct order_id) as total_orders,
-        avg(total_payment_value)::numeric(12, 2) as avg_order_value,
-        avg(delivery_days) as avg_delivery_days,
-        avg(is_late_delivery) * 100 as late_delivery_rate
-    from orders_enriched
-    group by 1, 2
+        order_id,
+        count(*) as item_count,
+        count(distinct product_id) as distinct_product_count,
+        count(distinct seller_id) as distinct_seller_count,
+        sum(item_price) as total_item_price,
+        sum(freight_value) as total_freight_value,
+        sum(total_item_value) as total_item_value,
+    from order_items_enriched
+    group by order_id
+),
+
+category_rank as (
+    select
+        order_id,
+        en_category_name,
+        sum(total_item_value) as category_revenue,
+        rank() over (
+            partition by order_id order by category_revenue desc
+            ) as rnk
+    from order_items_enriched
+    group by order_id, en_category_name
+),
+
+primary_category as (
+    select
+        order_id,
+        en_category_name as primary_category
+    from category_rank
+    where rnk = 1
 )
 
-SELECT
-    month,
-    year,
-    total_orders,
-    round(total_revenue, 2) as total_revenue,
-    round(avg_order_value, 2) as avg_order_value,
-    round(avg_delivery_days, 2) as avg_delivery_days,
-    round(late_delivery_rate, 2) as late_delivery_rate
-from monthly_sales
-order by month, year
+
+SELECT 
+    -- order information
+    oe.order_id,
+    oe.customer_id,
+    oe.customer_unique_id,
+    oe.order_status,
+    oe.order_purchase_timestamp,
+    cast(oe.order_purchase_timestamp as date) as order_date,
+    oe.order_year,
+    oe.order_month,
+    oe.customer_city,
+    oe.customer_state,
+    oe.total_payment_value,
+    oe.num_payments,
+    oe.max_installments,
+    oe.used_credit_card,
+    oe.used_voucher,
+    oe.used_boleto,
+    oe.used_debit_card,
+    oe.avg_review_score,
+    oe.most_recent_review_date,
+    oe.estimated_delivery_days,
+    oe.delivery_days,
+    oe.approval_days,
+    oe.is_late_delivery,
+    oe.is_delivered,
+
+    -- order item information
+    coalesce(oi.item_count, 0) as item_count,
+    coalesce(oi.distinct_product_count, 0) as distinct_product_count,
+    coalesce(oi.distinct_seller_count, 0) as distinct_seller_count,
+    coalesce(oi.total_item_price, 0) as total_item_price,
+    coalesce(oi.total_freight_value, 0) as total_freight_value,
+    coalesce(oi.total_item_value, 0) as total_item_value,
+    pc.primary_category
+
+from orders_enriched oe
+left join order_items oi
+    on oe.order_id = oi.order_id
+left join primary_category pc
+    on oe.order_id = pc.order_id

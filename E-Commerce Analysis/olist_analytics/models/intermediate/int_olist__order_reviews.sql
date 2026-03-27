3f3de348888c5366cc5cf92dd0350aad964b1b89
Isaac Rico <isaacarnell.rico@gmail.com>
2026-03-26 17:05:41 -0700

    quick fix for order reviews intermediate

--- E-Commerce Analysis/olist_analytics/models/intermediate/int_olist__order_reviews.sql
+++ E-Commerce Analysis/olist_analytics/models/intermediate/int_olist__order_reviews.sql
@@ -3,9 +3,10 @@
 with reviews as (SELECT * FROM {{ ref('stg_olist__reviews') }})
 
 SELECT 
+    review_id,
     order_id,
     avg(review_score) as avg_review_score,
     count(*) as num_reviews,
     max(review_creation_date) as most_recent_review_date
 FROM reviews
-GROUP BY order_id
+GROUP BY review_id, order_id

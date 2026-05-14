a05aafcc3ae45a1c99448ae36f7ed468ea4b6d0f
isaac-rico <isaacarnell.rico@gmail.com>
2026-05-13 18:14:05 -0700

    moved docker compose, finalized startup in RUNTHIS.py

--- Real-Time Trending Topic Detector/backend/RUNTHIS.py
+++ Real-Time Trending Topic Detector/backend/RUNTHIS.py
@@ -1,8 +1,30 @@
 # hi run this file to start all processes after running docker compose up -d in backend directory
+# if this doesn't work for you, run each file in its own terminal
 
+"""
+run order:
+BEFORE RUNNING PYTHON FILES: 
+fill out your .env file w/ postgres credentials and kafka config
+docker compose up -d (in backend directory)
+pip install -r requirements.txt
+
+and then run backend:
+1. Producer.py
+2. Consumer.py
+3. aggregate.py
+4. app.py
+
+for frontend:
+1. go to frontend directory
+2. npm install -> react typescript
+3. npm start
+4. open localhost:5173 in browser
+
+"""
 import subprocess
 import time
 import sys
+import os
 
 scripts = [
     ("Producer", "producer.py"),
@@ -10,6 +32,8 @@ scripts = [
     ("Aggregator", "aggregate.py"),
     ("FastAPI Server", "app.py")
 ]
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 processes = []
 
@@ -21,9 +45,16 @@ def start(name, path):
             creationflags=subprocess.CREATE_NEW_CONSOLE
         )
     else:
-        p = subprocess.Popen([
-            "gnome-terminal", "--", "python", path
-        ])
+        full_path = os.path.join(BASE_DIR, path)
+        script = f"""
+            tell application "Terminal"
+                activate
+                do script "python3 '{full_path}'"
+            end tell
+        """
+        p = subprocess.Popen(
+            ["osascript", "-e", script]
+        )
     return p
 
 try:
@@ -31,11 +62,14 @@ try:
         p = start(name, path)
         processes.append(p)
         time.sleep(2)
-        print("\n all processes started successfully \n")
-        while True:
-            time.sleep(1)
+        print(f"{name} started.")
+        
+    print("All processes started successfully.")
+    while True:
+        time.sleep(1)
 
 except KeyboardInterrupt:
     print("Shutting down...")
     for p in processes:
-        p.terminate()
+        print(f"Terminating process {p.pid}...")
+        p.kill()
